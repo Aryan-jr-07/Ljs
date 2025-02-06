427ddb4f3b2505a973ab06f6771f4ebf7a6ce8b1
Aryan-jr-07 <aryanjr2010@gmail.com>
2025-02-07 03:05:41 +0530

    function

--- index.js
+++ index.js
@@ -36,6 +36,118 @@
 // console.log(obj["fName"]);
 // console.log(obj)
 
-let age = 18;
-(age >=19) ? console.log("adult") : console.log("not adult");
+// let age = 35;
+// // (age >=19) ? console.log("adult") : console.log("not adult");
+// switch(age){
+//   case (age >=18):{
+//     console.log("adult");
+//     break
+//   }
+//   case(age >=25):{
+//     console.log("elder");
+//     break
+//   }
+//   case(age <=35):{
+//     console.log("baap")
+//     break
+//   }
+// }
+
+// let color = "red";
+// switch(color){
+//   case ("red"):{
+//     console.log("max wavelength");
+//     break
+
+//   }
+//   case ("blue"):{
+//     console.log("min wavelenght");
+//   }
+// }
+
+// let car = "city";
+// (car = "city")? console.log("true"): console.log("false");
+// console.log(bus)
+
+// for(let i =1; i<=10; ++i){
+//   console.log(i)
+// }
+
+// let i=1;
+// console.log(i++);  // => ye phele phele incriment nhi krega 2nd time se print&incriment krna suru krega
+// console.log(i++);
+// console.log(i++); 
+// console.log(++i);  // => this will print incriment and print from the 1st time
+
+// table of 2
+// for(let i=1; i<=10; i++){
+//   console.log(`2*${i} = ${i*2}`);
+// }
+
+// table of 9
+// for(let i=1; i<=10; ++i){
+//   console.log(`9*${i}= ${i*9}`)
+// }
+
+// for (let i=1; i<=20; i++){
+//   if(i ==12){
+//     break;  // break will do ki 11 k baad ka print nhi karega it means it will break the loop
+//     // continue; // continue mean it does not print 1 to 20 except 12 
+//   }else{
+//     console.log(i)
+//   }
+// }
+
+// while loop
+// let i =1;
+// while (i<=5){
+//   console.log(`value of i ${i}`);
+//   i+=2;
+// } 
+
+// let arrA = ['flash', 'batman', 'superman']
+// let arr =0;
+// while (arr < arrA.length){
+//   console.log(`value of ${arrA[arr]}`);
+//   arr+=1;
+// }
+
+// Function
+// function add(add1,add2){
+//   let result;
+//   result = add1+ add2;
+//   return result;
+// }
+// let sum = add(2,3);
+// console.log(sum);
+
+// function inn(in1,in2){
+//   return in1*in2;
+// }
+// const in3 = inn(3,2);
+// console.log(in3)
+
+// function dude(du1,du2){
+//   return du1*du2;
+// }
+// let ok = dude(10,9);
+// console.log(ok)
+
+// function div(di1,di2){
+//   return di1/di2;
+// }
+// let divide = div(990,2);
+// console.log(divide);
+
+
+
+
+
+
+
+
+
+
+
+
 
