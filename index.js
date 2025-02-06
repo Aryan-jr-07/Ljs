6e9b5dc9c9c8338114624fea368135cbc960a985
Aryan-jr-07 <aryanjr2010@gmail.com>
2025-02-06 10:22:44 +0530

    obj

--- index.js
+++ index.js
@@ -9,5 +9,30 @@
 // }
 // solve();
 
+// console.log(Math.round(4.5));
+// console.log("abcd".includes("b"));
+// console.log("abcd".indexOf("c"));
+// console.log(Math.max(1,2,3,3,4,5,6,7));
+
+let arr = [1,"1", true,undefined,null,[1,2]];
+// console.log(arr.length);
+// console.log(arr[7]);
+// arr.push();
+// console.log(arr); 
+
+// for (let index in arr){
+//     console.log(index,arr[index]);
+// }                /// this will give you the value of arry will it's index
+
+// for (let val of arr){
+//     console.log(val);
+// }    // this will give you the only the value of the arry 
 
 
+// let obj = {
+//   "fName": "Aryan",
+//   "lname": "jr",
+//   "school":"newton"
+// };
+// console.log(obj["fName"]);
+// console.log(obj)
